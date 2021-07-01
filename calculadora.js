4522a6bb4b3afde2f929d25ed86604669f7b45cc
Rosineia dos Santos <neyasantoss@gmail.com>
2021-07-01 20:32:41 -0300

    Adicionando pasta calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+
+const divisao= require("./divisao");
+const somar= require("./somar");
+const multiplicacao= require("./multiplicacao");
+const subtracao = require("./subtracao");
+
+
+
+
+
+
+
+
+
+console.log(divisao(100,4));
+console.log(somar(90,6));
+console.log(subtracao(19,2));
+console.log(multiplicacao(3,9));
